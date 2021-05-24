25bf04fc6025aada86d772f2748786547b32fd84
Brandon <bmacdonald1986@gmail.com>
2021-05-24 20:05:13 +0000

    Add player instance variables max_lives and lives, functions for losing a life and resetting the player state

--- src/components/Player.rb
+++ src/components/Player.rb
@@ -1,4 +1,14 @@
 class Player
-  def initialize
+  def initialize(max_lives)
+    @max_lives = max_lives
+    @lives = max_lives
+  end
+
+  def lose_life
+    @lives -= 1
+  end
+
+  def reset
+    @lives = @max_lives
   end
 end
